9e26d5c8341462dbd2cc4888e98e84c1095aa6ad
CT Cheng (Anna) <annacheng310@gmail.com>
2019-02-09 23:30:28 +0900

    All 8 tests passed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// npm install --save marked
+import marked from 'marked';
+
+let defaultInput = '# Welcome to my React Markdown Previewer!\n'+
+                    '## This is a sub-heading... \n'+
+                    '### And here\'s some other cool stuff: \n\n'+
+                    'Heres some code, `<div></div>`, between 2 backticks.\n\n'+
+                    '```\n// this is multi-line code:\n\n'+
+                    'function anotherExample(firstLine, lastLine) {\n'+
+                    '  if (firstLine == \'```\' && lastLine == \'```\') {\n'+
+                    '    return multiLineCode;\n  }\n}\n```\n\n'+
+                    'You can also make text **bold**... whoa!\n'+
+                    'Or _italic_.\n'+
+                    'Or... wait for it... **_both!_**\n'+
+                    'And feel free to go crazy ~~crossing stuff out~~.\n\n'+
+                    'There\'s also [links](https://www.freecodecamp.com), and\n'+
+                    '> Block Quotes!\n\n'+
+                    'And if you want to get really crazy, even tables:\n\n'+
+                    'Wild Header | Crazy Header | Another Header?\n'+
+                    '------------ | ------------- | ------------- \n'+
+                    'Your content can | be here, and it | can be here...\n'+
+                    'And here. | Okay. | I think we get it.\n\n'+
+                    '- And of course there are lists.\n'+
+                    '  - Some are bulleted.\n'+
+                    '     - With different indentation levels.\n'+
+                    '        - That look like this.\n\n\n'+
+                    '1. And there are numbererd lists too.\n'+
+                    '1. Use just 1s if you want! \n'+
+                    '1. But the list goes on...\n'+
+                    '- Even if you use dashes or asterisks.\n'+
+                    '* And last but not least, let\'s not forget embedded images:\n\n'+
+                    '![React Logo w/ Text](https://goo.gl/Umyytc)\n'
+
+marked.setOptions({
+      breaks: true
+    });
+
 class Editor extends React.Component {
   constructor(props) {
     super(props);
@@ -32,12 +69,12 @@ class Previewer extends React.Component {
     super(props);
   }
   render() {
+    let hmtlToShow = {__html:marked(this.props.input)};
     return (
       <div id="preview-window" className="window">
         <h2 className="title-bar"> Previewer </h2>
-        <div id="preview" className="textbox">
-        {this.props.input}
-        </div>
+        <div id="preview" className="textbox" dangerouslySetInnerHTML={hmtlToShow} />
+
       </div>
     );
   }
@@ -47,7 +84,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: 'Please enter your markdown here'
+      input: defaultInput
     }
     this.handleChange = this.inputChange.bind(this);
   }
